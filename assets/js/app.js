e389d5a2ee86d917476a823bc83e605e9fb3e278
Gaddafi <flisterz@gmail.com>
2013-07-21 08:35:49 +0800

    update algorithm, CSS and allow user input

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,42 @@
+
+var min_width = 175;
+
+
 $(document).ready(function(){
-	recalculate()
+
+	recalculate(min_width);
+
+	//Get value from user input
+	$("#update").click(function(){
+	 		min_width = $("#mingrid").val();
+			recalculate(min_width);
+	});
+
 });
 
 $(window).resize(function() {
-	recalculate()
+	recalculate(min_width);
 });
 
 
-function recalculate() {
+
+function recalculate(min_width) {
 
 	//Variables and calculations
-	//var grid_width = $('.grid').outerWidth();
-	var body_width = $(window).width();
 	var wrap_width = $(".wrapper").width();
-
-	var min_width = 220;
-	var col =  (wrap_width / min_width);
-	var col_rounded =  Math.round(wrap_width / min_width);
-	var grid_width = ((wrap_width/col_rounded)-40);
+	var col =  (wrap_width / min_width).toFixed(4);
+	var col_rounded =  Math.floor(wrap_width / min_width);
+	var grid_width = Math.floor(wrap_width/col_rounded);
 
 	$('.grid').width(grid_width);
 
 
 	//Displaying values
-	$('#wrw').html(wrap_width);
-	$('#ww').html(body_width);
+	$('#wrw').html(wrap_width + 'px');
 	$('#col').html(col);
 	$('#colrounded').html(col_rounded);
-	$('#grid').html(grid_width);
+	$('#grid').html(grid_width + 'px');
+	$('#mingrid').val(min_width);
 
 }
+
